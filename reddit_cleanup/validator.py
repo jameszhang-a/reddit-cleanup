20fc03fc4f3ae52d493f8abe47393ee479cab993
James Zhang <jameszhanga@gmail.com>
2022-03-03 22:38:57 -0600

    Add 1000 limit to validator

--- reddit_cleanup/validator.py
+++ reddit_cleanup/validator.py
@@ -1,10 +1,15 @@
+from email import message
 from prompt_toolkit.validation import Validator, ValidationError
 
 
 class NumberValidator(Validator):
     def validate(self, document):
         try:
-            int(document.text)
+            n = int(document.text)
+            if n > 1000:
+                raise ValidationError(
+                    message="cannot exceed 1000!", cursor_position=len(document.text)
+                )
         except ValueError:
             raise ValidationError(
                 message="Please enter a number", cursor_position=len(document.text)
